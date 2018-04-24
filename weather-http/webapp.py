d3adfd283bb237952ba2dbe36065a7db8d0f1045
Morgan Funtowicz <funtowiczmo@gmail.com>
2018-04-25 00:42:02 +0200

    Added intial layout for the frontend

--- weather-http/webapp.py
+++ weather-http/webapp.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
 from flask_graphql import GraphQLView
-from weather import schema
+from weather.graphql import schema
 
 app = Flask(__name__)
-
-
-app.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
-)
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema))
 
 
 @app.route('/')
