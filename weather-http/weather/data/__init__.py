627ad0fcaec6dc37107384047c0d3776667b224e
Morgan Funtowicz <morgan.funtowicz@naverlabs.com>
2018-12-03 16:21:45 +0100

    Remove checking for the same thread on the SQLite engine

--- weather-http/weather/data/__init__.py
+++ weather-http/weather/data/__init__.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('sqlite:///database.sqlite3', convert_unicode=True, echo=True)
-session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+engine = create_engine('sqlite:///database.sqlite3?check_same_thread=False', convert_unicode=True, echo=True)
+session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=True))
 
 Base = declarative_base()
 Base.query = session.query_property()
